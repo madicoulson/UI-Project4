7de2b357f75400a958f1bd09393c58cc5f05fe0c
madicoulson <coulsomj@mail.uc.edu>
2024-11-26 18:03:08 -0500

    added Text

---
+++ get-out-there/src/lib/components/Text.svelte
@@ -0,0 +1,23 @@
+<script>
+	export let type = 'default';
+	export let size = 'default';
+</script>
+
+<span
+	class="text"
+	class:default={type === 'default'}
+    class:bold={size === 'large'}
+>
+	<slot></slot>
+</span>
+
+<style>
+	.text {
+		color: black;
+		font-size: 16px;
+	}
+
+    .bold {
+        font-weight: bold;
+    }
+</style>
