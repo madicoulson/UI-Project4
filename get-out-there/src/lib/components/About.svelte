e827bd0e60c5fd8ef61e966ee8db24c783ff860b
madicoulson <coulsomj@mail.uc.edu>
2024-12-08 13:11:09 -0500

    added about section

---
+++ get-out-there/src/lib/components/About.svelte
@@ -0,0 +1,37 @@
+<script>
+    import Text from '$lib/components/Text.svelte';
+    import Header from '$lib/components/Header.svelte';
+    import Button from '$lib/components/Button.svelte';
+</script>
+
+<div class="box">
+    <Header type='subheader'>
+        About
+        <span class="italic">Get Out There!</span>
+    </Header>
+    <div class="text-padding">
+    <Text> Get Out There is a platform designed for anyone looking to explore their city, whether it’s a familiar place or somewhere new. 
+    It offers a space to discover local events, see how many people are attending, and join a group of others if you’d like 
+    to attend together.</Text>
+    </div>
+
+    <Text>The platform features Highlighted Events, along with various other selection types and features. In the profile section, 
+    users can add events they discover and contribute them to the website, connect with other users, explore pages of other cities 
+    through the Explore tab, and join or create groups. Get Out There is a fantastic way to connect with others—start by checking out 
+    the local events below!</Text>
+</div>
+
+<style>
+    .italic {
+        font-style:oblique;
+    }
+    .box{
+        padding: 20px;
+        display: flex;
+        flex-direction: column;
+    }
+    .text-padding {
+        padding-top: 20px;
+        padding-bottom: 20px;
+    }
+</style>
--- get-out-there/src/routes/+page.svelte
+++ get-out-there/src/routes/+page.svelte
@@ -4,10 +4,13 @@
     import ImageBox from "$lib/components/ImageBox.svelte";
     import profile from '$lib/components/img/profile.svg';
     import CheckOutMore from "$lib/components/CheckOutMore.svelte";
+    import About from "$lib/components/About.svelte";
 </script>
 
 <div class="component">
     <HeaderCard></HeaderCard>
+    <About></About>
+    <hr class="solid">
     <HighlightedEvents></HighlightedEvents>
     <hr class="solid">
     <CheckOutMore></CheckOutMore>
